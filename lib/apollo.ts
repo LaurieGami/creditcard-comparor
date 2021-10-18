72ef047959fc0d80187639d31a2ff43bbfc56f90
Juan Cortez <juan_cortez@utexas.edu>
2021-10-18 13:18:25 -0500

    Tmp add EC2 api url

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.NODE_ENV === 'production' ? 'http://3.85.210.229/api/graphql' : 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
 });
 
