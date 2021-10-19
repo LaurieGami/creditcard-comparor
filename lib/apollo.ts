e6dd1a39e183e3cfa6b9118f51d2b17352aec489
Juan Cortez <juan_cortez@utexas.edu>
2021-10-19 16:21:27 -0500

    Expose next js environment browser variable correctly

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: process.env.NODE_ENV === 'production' ? process.env.GRAPH_QL_URI : 'http://localhost:3000/api/graphql',
+  uri: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_GRAPH_QL_URI : 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
 });
 
