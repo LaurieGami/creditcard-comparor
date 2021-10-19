bc68e1566c5769e4e4ae5598e240155ed95bfe73
Juan Cortez <juan_cortez@utexas.edu>
2021-10-19 15:53:09 -0500

    Add link to production graphql api

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: process.env.NODE_ENV === 'production' ? 'http://3.85.210.229/api/graphql' : 'http://localhost:3000/api/graphql',
+  uri: process.env.NODE_ENV === 'production' ? process.env.GRAPH_QL_URI : 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
 });
 
