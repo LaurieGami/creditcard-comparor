b85fc5f791e763f4d981da074a504c1eb080cb03
lauriegami <laurievilleton@gmail.com>
2021-10-19 09:36:18 -0700

    Created a header and footer component and installed Material UI

---
+++ components/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div>Logo</div>
+            <nav>
+                <ul>
+                    <li>Credit Cards</li>
+                    <li>Merchants</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Header from "components/Header";
+import Footer from "components/Footer/Footer";
+
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+      <>
+        <Header />
+        <div>{children}</div>
+        <Footer />
+      </>
+    );
 };
 
 export default Layout;
