20d9c3ea4e78cf0d35aeb5e85eb8f6eb5f8de86a
Juan Cortez <juan_cortez@utexas.edu>
2021-10-20 12:23:04 -0500

    Add cards in a container

--- components/CreditCardSelector/index.tsx
+++ components/CreditCardSelector/index.tsx
@@ -4,108 +4,96 @@ import { useQuery, useMutation } from '@apollo/client';
 // import { AddCreditCard, AddCreditCardVariables } from './__generated__/AddCreditCard';
 // import { AllDataQuery } from './__generated__/AllDataQuery';
 import { GET_ALL_DATA } from '../Admin/AdminQueries';
-import { Card, CardContent, Button, Checkbox, FormControl, Select, MenuItem, TextField, CardHeader } from '@mui/material';
+import { Card, CardContent, Button, Grid, Checkbox, FormControl, Select, MenuItem, TextField, CardHeader, Box } from '@mui/material';
 
 export default function Admin() {
-    const { data, loading, error, refetch } = useQuery(GET_ALL_DATA);
+  const { data, loading, error, refetch } = useQuery(GET_ALL_DATA);
 
-    const [selectedCreditCards, setSelectedCreditCards] = useState([]);
-    const [selectedMerchant, setSelectedMerchant] = useState('');
-    const [shoppingValue, setShoppingValue] = useState(0);
+  const [selectedCreditCards, setSelectedCreditCards] = useState([]);
+  const [selectedMerchant, setSelectedMerchant] = useState('');
+  const [shoppingValue, setShoppingValue] = useState(0);
 
-    // useEffect(() => {
-    //     if (addCreditCardData) {
-    //         setCreditCardName('');
-    //         refetch();
-    //     }
-    // }, [addCreditCardData]);
+  // useEffect(() => {
+  //     if (addCreditCardData) {
+  //         setCreditCardName('');
+  //         refetch();
+  //     }
+  // }, [addCreditCardData]);
 
-    const handleCreditCardsSelection = (event) => {
-        if (event.target.checked) {
-            if (!selectedCreditCards.includes(event.target.value)) {
-                setSelectedCreditCards([...selectedCreditCards, event.target.value]);
-            }
-        } else {
-            setSelectedCreditCards(selectedCreditCards.filter(creditCard => creditCard !== event.target.value));
-        }
-    };
+  const handleCreditCardsSelection = (event) => {
+    if (event.target.checked) {
+      if (!selectedCreditCards.includes(event.target.value)) {
+        setSelectedCreditCards([...selectedCreditCards, event.target.value]);
+      }
+    } else {
+      setSelectedCreditCards(selectedCreditCards.filter((creditCard) => creditCard !== event.target.value));
+    }
+  };
 
-    const handleMerchantSelection = (event) => {
-        setSelectedMerchant(event.target.value);
-    };
+  const handleMerchantSelection = (event) => {
+    setSelectedMerchant(event.target.value);
+  };
 
-    const handleShoppingValue = (event) => {
-        setShoppingValue(event.target.value);
-    };
+  const handleShoppingValue = (event) => {
+    setShoppingValue(event.target.value);
+  };
 
-    const handleSubmit = useCallback(
-        (event) => {
-            event.preventDefault();
-        },
-        []
-    );
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+  }, []);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Oh no... {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
 
-    return (
-        <div>
-            <Head>
-                <title>Credit Card Selector</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <Box padding={2}>
+      <Head>
+        <title>Credit Card Selector</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <div>
-                <h2>Select the credit cards you'd like to compare:</h2>
+      <div>
+        <h2>Select the credit cards you'd like to compare:</h2>
 
-                <form onSubmit={handleSubmit}>
-                    {data.creditCards.map((card) => (
-                        <Card key={card.id}>
-                            <CardContent>
-                                <CardHeader title={card.creditCardName}>
-                                    <h3>{card.creditCardName}</h3>
-                                </CardHeader>
-                                <div>Image Placeholder</div>
-                                <Checkbox id={card.id} name={card.creditCardName} value={card.id} onChange={handleCreditCardsSelection} />
-                            </CardContent>
-                        </Card>
-                    ))}
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {data.creditCards.map((card) => {
+              return (
+                <Grid item key={card.id}>
+                  <Card>
+                    <CardContent>
+                      <CardHeader title={card.creditCardName}>
+                        <h3>{card.creditCardName}</h3>
+                      </CardHeader>
+                      <div>Image Placeholder</div>
+                      <Checkbox id={card.id} name={card.creditCardName} value={card.id} onChange={handleCreditCardsSelection} />
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
 
+          <h2>Choose a Merchant from this list:</h2>
 
-                    <h2>Choose a Merchant from this list:</h2>
+          <FormControl>
+            <Select labelId="merchant" id="merchants-selection" value={selectedMerchant} onChange={handleMerchantSelection}>
+              <MenuItem value="Starbucks">Starbucks</MenuItem>
+              <MenuItem value="Costco">Costco</MenuItem>
+              <MenuItem value="Walmart">Walmart</MenuItem>
+            </Select>
+          </FormControl>
 
-                    <FormControl>
-                        <Select
-                            labelId="merchant"
-                            id="merchants-selection"
-                            value={selectedMerchant}
-                            onChange={handleMerchantSelection}
-                        >
-                            <MenuItem value="Starbucks">Starbucks</MenuItem>
-                            <MenuItem value="Costco">Costco</MenuItem>
-                            <MenuItem value="Walmart">Walmart</MenuItem>
-                        </Select>
-                    </FormControl>
+          <h2>How much will you be spending there?</h2>
+          <TextField id="shopping-value" name="shopping-value" value={shoppingValue} type="number" onChange={handleShoppingValue} />
 
-                    <h2>How much will you be spending there?</h2>
-                    <TextField
-                        id="shopping-value"
-                        name="shopping-value"
-                        value={shoppingValue}
-                        type="number"
-                        onChange={handleShoppingValue}
-                    />
+          {/* <input type="number" id="shopping-value" name="shopping-value" value={shoppingValue} onChange={handleShoppingValue} /> */}
 
-                    {/* <input type="number" id="shopping-value" name="shopping-value" value={shoppingValue} onChange={handleShoppingValue} /> */}
-
-                    <Button
-                        type="submit"
-                        variant="contained"
-                    >
-                        Search
-                    </Button>
-                </form>
-            </div>
-        </div>
-    );
+          <Button type="submit" variant="contained">
+            Search
+          </Button>
+        </form>
+      </div>
+    </Box>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,26 @@
-import "styles/globals.css";
-import Layout from "components/Layout";
-import { ApolloProvider } from "@apollo/client";
-import apolloClient from "lib/apollo";
+import 'styles/globals.css';
+import Layout from 'components/Layout';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from 'lib/apollo';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({});
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={apolloClient}>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
